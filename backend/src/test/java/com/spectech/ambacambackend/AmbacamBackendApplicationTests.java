e664cb3e1f87e25e9b606579a6a8a53dce8e4db2
Lyonnel Dzotang <dtlyonnel@gmail.com>
2018-05-17 22:56:21 +0100

    Etat initial du backend

---
+++ backend/src/test/java/com/spectech/ambacambackend/AmbacamBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spectech.ambacambackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AmbacamBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
